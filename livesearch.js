d57180c17186c2785ff66d446055d6f5f7aab13b
Matthew Heller <matt@devbreeze.com>
2011-02-10 20:51:37 -0600

    Simple working example.

---
+++ livesearch.js
@@ -0,0 +1,37 @@
+jQuery.fn.liveUpdate = function(list, itemSelector, contentSelector){
+	list = jQuery(list);
+
+	if ( list.length ) {
+		var rows = list.children(itemSelector),
+			cache = rows.map(function(){
+				return jQuery(contentSelector, this).get(0).firstChild.nodeValue.toLowerCase();
+			});
+			
+		this
+			.keyup(filter).keyup()
+			.parents('form').submit(function(){
+				return false;
+			});
+	}
+		
+	return this;
+		
+	function filter(){
+		var term = jQuery.trim( jQuery(this).val().toLowerCase() ), scores = [];
+		
+		if ( !term ) {
+			rows.show();
+		} else {
+			rows.hide();
+
+			cache.each(function(i){
+				var score = this.score(term);
+				if (score > 0) { scores.push([score, i]); }
+			});
+
+			jQuery.each(scores.sort(function(a, b){return b[0] - a[0];}), function(){
+				jQuery(rows[ this[1] ]).show();
+			});
+		}
+	}
+};
