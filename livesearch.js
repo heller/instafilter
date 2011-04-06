51e40d87e53e5ceaf2d09e2a7638043ac5af947a
Matthew Heller <matt@devbreeze.com>
2011-04-05 19:59:01 -0500

    Move function out of other function. El oh el.

--- livesearch.js
+++ livesearch.js
@@ -1,42 +1,40 @@
 (function ($) { 
   $.fn.liveUpdate = function(list, itemSelector, contentSelector){
+    list = $(list);
 
-  list = $(list);
+    rows = list.children(itemSelector);
+    
+    cache = rows.map(function(){
+      return $(contentSelector, this).get(0).firstChild.nodeValue.toLowerCase();
+    });
 
-	if ( list.length ) {
-		var rows = list.children(itemSelector),
-			cache = rows.map(function(){
-				return $(contentSelector, this).get(0).firstChild.nodeValue.toLowerCase();
-			});
-			
 		this
       .bind('keyup.instafilter', filter)
       .keyup()
 			.parents('form').submit(function(){
 				return false;
 			});
-	}
-		
-	return this;
-		
-	function filter(){
-		var term = $.trim( $(this).val().toLowerCase() ), scores = [];
-		
-		if ( !term ) {
-			rows.show();
-		} else {
-			rows.hide();
 
-			cache.each(function(i){
-				var score = this.score(term);
-				if (score > 0) { scores.push([score, i]); }
-			});
+    return this;
+  };
 
-			$.each(scores.sort(function(a, b){return b[0] - a[0];}), function(){
-				$(rows[ this[1] ]).show();
-			});
-		}
-	}
-};
+  function filter(){
+    var term = $.trim( $(this).val().toLowerCase() ), scores = [];
+    
+    if ( !term ) {
+      rows.show();
+    } else {
+      rows.hide();
+
+      cache.each(function(i){
+        var score = this.score(term);
+        if (score > 0) { scores.push([score, i]); }
+      });
+
+      $.each(scores.sort(function(a, b){return b[0] - a[0];}), function(){
+        $(rows[ this[1] ]).show();
+      });
+    }
+  }
 
 })(jQuery);
